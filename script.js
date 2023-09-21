b3431fad459834224b02bc0feadcc7073021f857
Sime Jadrijev <sime.jadrijev@gmail.com>
2023-09-22 00:38:27 +0200

    Nadograđena 'O NAMA' sekcija (counter animation)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Responsive menu
 const menuIcon = document.getElementById("menuIcon");
 const closeIcon = document.getElementById("closeIcon");
 const menuList = document.getElementById("menuList");
@@ -13,3 +14,26 @@ closeIcon.addEventListener("click", () => {
     menuIcon.style.display = "block";
     closeIcon.style.display = "none";
 });
+
+//Counter animation
+let brojKlijenata = document.querySelector("#brojKlijenata");
+let godineIskustva = document.querySelector("#godineIskustva");
+
+function animateCounter(element, endValue, duration) {
+    let startValue = 0;
+    let intervalDuration = duration / endValue;
+
+    let counter = setInterval(() => {
+        startValue += 1;
+        element.textContent = startValue;
+
+        if (startValue >= endValue) {
+            clearInterval(counter);
+        }
+    }, intervalDuration);
+}
+
+animateCounter(brojKlijenata, parseInt(brojKlijenata.getAttribute("data-val")), 1);
+animateCounter(godineIskustva, parseInt(godineIskustva.getAttribute("data-val")), 4000);
+
+

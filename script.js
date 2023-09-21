fb3d0a12e343c4ddc501d5e1f8e8843b93832273
Sime Jadrijev <sime.jadrijev@gmail.com>
2023-09-21 22:57:45 +0200

    Napravljena o nama sekcija i promijenjeni itemi unutar menuLista za navigaciju. I dalje podložno promjenama

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ const closeIcon = document.getElementById("closeIcon");
 const menuList = document.getElementById("menuList");
 
 menuIcon.addEventListener("click", () => {
-    menuList.style.display = "block";
     menuList.classList.add("active");
     menuIcon.style.display = "none";
     closeIcon.style.display = "block";
 });
 
 closeIcon.addEventListener("click", () => {
-    menuList.style.display = "none";
     menuList.classList.remove("active");
     menuIcon.style.display = "block";
     closeIcon.style.display = "none";
